d47da057ee614b4cae28698017c96f4dd9352578
Michel Kim <kim98.michel@gmail.com>
2023-07-24 10:16:48 -0300

    Initial commit

---
+++ Joking-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Joking-iOS
+//
+//  Created by Michel Jaejin Kim on 24/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
